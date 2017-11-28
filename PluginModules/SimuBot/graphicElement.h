db2a70ea9349aefec3628a2bfece26867e47e0a1
laguiche <ste.guichard@gmail.com>
2017-11-28 19:02:10 +0100

    Mise à jour SimuBot - A*
    Diverses améliorations de SimuBot (deux placements d'init, prise en compte du theta d'init) et intégration de l'algorithme de pathfinding A*

--- PluginModules/SimuBot/graphicElement.h
+++ PluginModules/SimuBot/graphicElement.h
@@ -13,6 +13,19 @@
 static const double Pi = 3.14159265358979323846264338327950288419717;
 static const double TwoPi = 2.0 * Pi;
 
+class Coord
+
+{
+public:
+    Coord();
+    Coord(float _x, float _y, float _teta, bool _ortho);
+    void init(float _x, float _y, float _teta, bool _ortho);
+    float x;
+    float y;
+    float teta;
+    bool ortho;
+};
+
 class GraphicElement : public QObject , public QGraphicsPolygonItem
 {
  Q_OBJECT
@@ -36,7 +49,9 @@ private:
     qreal x_reel;
     qreal y_reel;
     qreal x_offset, y_offset, angle_offset;
+    qreal x_asserv_init, y_asserv_init, angle_asserv_init;
     QColor color;
+
 
 
 public:
@@ -46,6 +61,8 @@ public:
     qreal getX_terrain(void);
     qreal getY_terrain(void);
     bool isRelativToBot;
+   int sensOrtho;
+   void setAsservInit(qreal x_init, qreal y_init, qreal angle_init);
 
 signals:
     void center(qreal x_new, qreal y_new,float speed);
@@ -53,6 +70,7 @@ signals:
 public slots:
     void replace(qreal x_new, qreal y_new,float speed);
     void raz(qreal x_new, qreal y_new, qreal angle_new);
+    void raz(Coord cTerrain, Coord cAsserv);
     void display_XY(qreal x_reel_new,qreal y_reel_new);
     //void display_Y(qreal y_reel_new);
     void display_theta(qreal theta_reel_new);
