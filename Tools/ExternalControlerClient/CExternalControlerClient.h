a9028a4053b8835869b444f3dd09c8068978d13f
NicoFarmer <nicolas.santiago77.deal@gmail.com>
2019-06-21 00:44:04 +0200

    #14 [Tools] Ajout de la doc d'utilisation du client ExternalControler

--- Tools/ExternalControlerClient/CExternalControlerClient.h
+++ Tools/ExternalControlerClient/CExternalControlerClient.h
@@ -3,6 +3,33 @@
 
 #include <QTcpSocket>
 
+// Cette classe est un client TCP pour le module serveur TCP ExternalControler
+// Le client permet de dialoguer avec le serveur ExternalControler d'un autre outil
+
+// Utilisation :
+// #include "CExternalControlerClient.h"
+// ....
+// Création d'une instance du client
+//   CExternalControlerClient m_external_controler_client;
+
+// Initialisation du client :
+// Indiquer l'addresse IP et le port du serveur
+//   m_external_controler_client.open("127.0.0.1", 1234);
+
+// API du client :
+//   int err;
+//   Lecture d'une data distante
+//   qDebug() << m_external_controler_client.readData("PosY_robot", &err).toString();
+//   if (err != 0) qDebug() << "Error code" << err;
+
+//   Ecriture d'une data distante
+//   m_external_controler_client.writeData("PosY_robot", "136.9", &err);
+//   if (err != 0) qDebug() << "Error code" << err;
+
+//   Création d'une nouvelle data dans le datamanager distant
+//   m_external_controler_client.createData("new_data", "Hello 123", &err);
+//   if (err != 0) qDebug() << "Error code" << err;
+
 class CExternalControlerClient : public QObject
 {
     Q_OBJECT
