b23b23cb9d47b9079b425ba58d4faa570d531c19
NicoFarmer <nicolas.santiago77.deal@gmail.com>
2024-08-22 18:57:43 +0200

    #82 Force le style et la palette de l'application pour compatibilité Robuntu22.04 et RobianPi V2.0
    Migration sous RobianPi V2.0 (64bits) avec Qt 5.15.8

--- main.cpp
+++ main.cpp
@@ -1,12 +1,32 @@
+#include <QStyleFactory>
+#include <QToolTip>
+#include <QStyle>
 #include <QApplication>
 #include "CApplication.h"
+
+void setApplicationApparence();
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    setApplicationApparence();
     CApplication LaBotBox;
     LaBotBox.run();
 
     return a.exec();
     //
+}
+
+
+// force l'apparence de l'application (nécessaire pour compatibilité PC/Raspberry)
+void setApplicationApparence()
+{
+    // sur Rasbperry OS, le style par défaut ne permet pas d'afficher la couleur sur les boutons
+    // => force le style à Fusion (par défaut sur PC mais pas sur Raspberry)
+    QApplication::setStyle(QStyleFactory::create("Fusion"));  // pour récupérer la liste des styles possibles sur la plateforme courante : "qDebug() << QApplication::style();"
+
+    // sur Rasberry OS, la couleur du tooltip est blanche sur fond jaune et ne se voit pas
+    QPalette palette = QToolTip::palette();
+    palette.setColor(QPalette::ToolTipText, Qt::black);
+    QToolTip::setPalette(palette);
 }
